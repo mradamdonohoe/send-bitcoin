5c40661aa7d536e6cc96aff55818bdc529ab3641
Adam Donohoe <Adam.Donohoe@sysnetgs.com>
2022-05-19 15:16:13 +0100

    AD - more functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
-//bookmark vid 1 10:48
+//bookmark vid 2
 
 use std::io;
+use rand::Rng;
+
+fn send_bitcoin() {
+    println!("we shall send some bitcoin");
+    println!("send to who?");
+    let clients = vec!["homer", "marge", "bart", "lisa"];
+    
+    for client in &clients {
+        print!("{}", client);
+    }
+
+    let mut recipient = String::new();
+    io::stdin().read_line(&mut recipient); 
+
+    if clients.contains(&recipient.trim()) {
+        println!("how much bitcoin?");
+
+        let mut amount = String::new();
+        io::stdin().read_line(&mut amount);
+
+        println!("you sent {} bitcoin to {}", amount.trim(), recipient);
+    } else {
+        println!("person is not in your contacts");
+    }
+}
+
+fn receive_bitcoin() {
+    println!("we shall receive some bitcoint");
+
+    let amount = rand::thread_rng().gen_range(1, 10);
+    println!("you just received {} bitcoin", amount);
+}
+
+fn exit_console() {
+    println!("Invalid option must be r or s");
+}
 
 fn console(){
     println!("\nlets have fun with bitcoin\n");
@@ -8,7 +44,15 @@ fn console(){
 
     let mut command = String::new();
     
-    io::stdin().read_line(&mut command);    
+    io::stdin().read_line(&mut command); 
+
+    if command.trim().eq("s") {
+        send_bitcoin();
+    }  else if command.trim().eq("r") {
+        receive_bitcoin();
+    } else {
+        exit_console();
+    }
 }
 
 fn main() {
